b0ba538e6fc80261b9bc063c9bf1d44a1b96ad2c
makun <michaelmakun@aliyun.com>
2017-03-22 19:58:52 +0800

    fix productlist

--- db/migrate/20170322071136_fix_product_lists.rb
+++ db/migrate/20170322071136_fix_product_lists.rb
@@ -1,5 +1,5 @@
 class FixProductLists < ActiveRecord::Migration[5.0]
   def change
-    change_column :product_lists, :product_price, 'integer USING CAST(incident_id AS integer)'
+    change_column :product_lists, :product_price, 'integer USING CAST'
   end
 end
